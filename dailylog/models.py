7f1e5926e267331b88bb99755c8e1537d87237a9
dyyi <dongyoun.yi@gmail.com>
2020-04-06 22:59:46 +0900

    errata in settings.AUTH_USER_MODEL

--- dailylog/models.py
+++ dailylog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class PraryLog(models.Model):
-    author = models.ForeignKey('settings.AUTH_USER_MODEL', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     pray_start = models.DateTimeField()
     pray_end = models.DateTimeField()
     updated_date = models.DateTimeField(default=timezone.now)
