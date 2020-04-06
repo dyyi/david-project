b328b3335f26d1bf0c3420c0b108f08bd9dc5451
dyyi <dongyoun.yi@gmail.com>
2020-04-06 23:10:38 +0900

    Add date field for logging date'

--- dailylog/models.py
+++ dailylog/models.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from datetime import date
 
 class PrayLog(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateField(default=date.today)
     pray_start = models.DateTimeField()
     pray_end = models.DateTimeField()
     updated_date = models.DateTimeField(default=timezone.now)
