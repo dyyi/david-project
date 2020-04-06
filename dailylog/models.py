ca84a7b76f0685ed3597e0af6ec46375942ef17e
dyyi <dongyoun.yi@gmail.com>
2020-04-06 23:03:33 +0900

    Fix errata in model name PrayLog

--- dailylog/models.py
+++ dailylog/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class PraryLog(models.Model):
+class PrayLog(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     pray_start = models.DateTimeField()
     pray_end = models.DateTimeField()
