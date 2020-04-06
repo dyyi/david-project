60272e08f7d3793a9350924b610e15d86184554a
dyyi <dongyoun.yi@gmail.com>
2020-04-07 00:03:01 +0900

    Passing over queryset data to template test

--- dailylog/views.py
+++ dailylog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import PrayLog
 
 def praylog_list(request):
-    return render(request, 'dailylog/praylog.html', {})
+    praylogs = PrayLog.objects.all().order_by('date')
+    return render(request, 'dailylog/praylog.html', {'praylogs':praylogs})
